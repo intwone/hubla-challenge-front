d7c10be3463ae694f50236926a138b7a60210403
intwone <cassiointw1993@gmail.com>
2022-10-07 10:34:59 -0300

    change index route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 export default function Routers() {
   return (
     <Routes>
-      <Route path="/upload" element={<UploadPage />} />
+      <Route path="/" element={<UploadPage />} />
       <Route path="/transactions" element={<TransactionPage />} />
     </Routes>
   );
