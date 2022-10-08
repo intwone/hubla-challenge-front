b1f276c710ea573069f3223afc90d3d4fd8c339a
intwone <cassiointw1993@gmail.com>
2022-10-08 10:00:12 -0300

    add requests to list transactions

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,14 @@
 /// <reference types="cypress" />
 
 import 'cypress-file-upload';
+import 'cypress-xpath';
+
+const baseUrl = 'http://localhost:4000';
+
+Cypress.Commands.add('listAllTransactions', () => {
+  cy.request('GET', `${baseUrl}/transaction`);
+});
+
+Cypress.Commands.add('listTransactionsByType', (type: number) => {
+  cy.request('GET', `${baseUrl}/transaction/${type}`);
+});
